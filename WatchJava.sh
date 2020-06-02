ea43b2be58f3e54f29084163052f53d759290dbf
TatsuyaOgawa <ogawa@itpm-gk.com>
2020-06-02 21:50:30 +0900

    20200602_ko1

--- WatchJava.sh
+++ WatchJava.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "*** OldUse(%) ***"
-java -jar jmx-cmdclient-0.1.1.jar localhost:8766 "java.lang:type=MemoryPool,name=G1 Old Gen" "Usage" | tail -n 1 | awk -F',' '{printf( "scale=5;%s / %s * 100\n", $5, $2 );}' | bc
+java -jar jmx-cmdclient-0.1.1.jar localhost:8768 "java.lang:type=MemoryPool,name=G1 Old Gen" "Usage" | tail -n 1 | awk -F',' '{printf( "scale=5;%s / %s * 100\n", $5, $2 );}' | bc
 
 echo "*** FullGC Count ***"
-java -jar jmx-cmdclient-0.1.1.jar localhost:8766 "java.lang:name=G1 Old Generation,type=GarbageCollector" "CollectionCount" | tail -n 1 | cut -d',' -f 2
+java -jar jmx-cmdclient-0.1.1.jar localhost:8768 "java.lang:name=G1 Old Generation,type=GarbageCollector" "CollectionCount" | tail -n 1 | cut -d',' -f 2
 
 echo "*** FullGC Time(ms) ***"
-java -jar jmx-cmdclient-0.1.1.jar localhost:8766 "java.lang:name=G1 Old Generation,type=GarbageCollector" "CollectionTime" | tail -n 1 | cut -d',' -f 2
+java -jar jmx-cmdclient-0.1.1.jar localhost:8768 "java.lang:name=G1 Old Generation,type=GarbageCollector" "CollectionTime" | tail -n 1 | cut -d',' -f 2
 
 exit
 
